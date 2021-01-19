c566a145e7dbf936d1cc72b4371608f20f0ca28a
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-01-19 16:02:48 -0600

    Add orgzr.php

---
+++ orgzr.php
@@ -0,0 +1,14 @@
+<?php
+    $handle = opendir($argv[1]);
+    // Creates a new directory where files will be stored
+    $new_dir = "$argv[1]/$argv[3]"; 
+    mkdir($new_dir);
+    while (false !== ($file = readdir($handle))) {
+        $parts = explode('.', $file);
+        // As of PHP 8 str_contains() can be used
+        if ($parts[count($parts)-1] == $argv[2]) {
+            rename("$argv[1]/$file", "$new_dir/$file");
+        }
+    }
+    closedir($handle);
+?>
